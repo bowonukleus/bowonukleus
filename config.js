f4e890c55ac6c4f26ccb9b2b4293c658ed507ad0
bowonukleus <satriyo.wibowo@outlook.com>
2017-06-07 10:02:10 -0700

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ 'TelegramProductionURL': 'https://t.me/serangunitedbot/', // do not forget trailing "/" !!
+ 'TelegramToken': '344509141:AAEHUSOsuRReLLHQnzfB3c6sUNB-Pqxunz4', // Telegram token
+ 'googleSheetKey': '0B0Os4PYKjRiyZlJ1QkVjcUNRcjQ', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+}
